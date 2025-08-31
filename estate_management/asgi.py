b45fea12ad45fad60597400130bb315ad4f9acc8
Thompson Falowo <thompsongboyega2019@gmail.com>
2025-08-31 21:32:31 +0100

    Deleted the previous project and app, then setup a new project and app. And I also installed djangorestframework

--- estate_management/asgi.py
+++ estate_management/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for estate project.
+ASGI config for estate_management project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estate_management.settings')
 
 application = get_asgi_application()
